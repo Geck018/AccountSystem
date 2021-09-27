283dc8c1764c1f2c8e25c9675ebcee3813c72e47
Geck018 <marriesteyngcko@gmail.com>
2021-09-27 09:59:43 +0200

    Commit via Intellij. Progress: Whitelabel error fixed(!!!) due to missing swagger-ui dependency. Moving on to unit testing.

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/RestServiceApplication.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/RestServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"za.ac.nwu.ac.web.sb.controller"})
+//@ComponentScan({"za.ac.nwu.ac.web.sb.controller"})
 public class RestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
