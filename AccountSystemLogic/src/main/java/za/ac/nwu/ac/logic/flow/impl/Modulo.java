07719c494f5cb2b4a027b185d55350fc2a7f910a
Geck018 <marriesteyngcko@gmail.com>
2021-09-27 12:52:58 +0200

    Commit via Intellij. Progress: Added unit test at FetchAccountTypeFlowImplTest.java, test success.

---
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/Modulo.java
@@ -0,0 +1,9 @@
+//package za.ac.nwu.ac.logic.flow.impl;
+//
+//public class Modulo
+//{
+//    public Integer doMod(int dividend, int divisor)
+//    {
+//        return dividend/divisor;
+//    }
+//}
